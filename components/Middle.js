e5bb546937628302d9906b7b41cef75ddd4ada43
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2022-10-18 08:59:48 +0530

    i

---
+++ components/Middle.js
@@ -0,0 +1,238 @@
+import React from "react";
+import Company from "../pages/company";
+import Link from "next/Link";
+
+
+const Middle = () => {
+  
+ 
+  return (
+    <>
+      <section className="text-white body-font bg-black">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 className="title-font sm:text-6xl text-3xl mb-4 font-medium text-white font-mono">
+              Don't Get REKT
+            </h1>
+            <h1 className="title-font sm:text-3xl text-3xl mb-4 font-medium text-white font-mono">
+              Whitehats are here to save you
+            </h1>
+          </div>
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <img
+              className="object-cover object-center rounded"
+              alt="hero"
+              src="https://www.vskills.in/certification/tutorial/wp-content/uploads/2013/05/introduction-white-hat-hacking-security.jpg"
+            />
+          </div>
+        </div>
+      </section>
+      <section className="text-white body-font bg-black px-4 py-10">
+        <div>
+          <div className="title-font sm:text-7xl text-3xl mb-4 font-medium text-white font-mono text-center p-5">
+            <span className="title-font sm:text-7xl text-3xl mb-4 font-medium text-white font-mono text-center p-1">
+              Programs
+            </span>
+            <span className="font-mono text-xl font-normal text-gray-500 dark:text-gray-400">
+              (active)
+            </span>
+          </div>
+          <div className="flex flex-row flex-wrap ">
+          <div className=" m-1 p-4 w-full max-w-sm bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <div className="flex items-baseline text-gray-900 dark:text-white">
+              <span className="text-4xl font-semibold font-mono">
+                Company Name
+              </span>
+            </div>
+            <div className="p-4">
+             
+              <h2 className=" mb-2 text-base text-gray-900 dark:text-white font-mono">
+                Details:
+              </h2>
+              <ul className="font-mono space-y-1 max-w-md list-disc list-inside text-gray-500 dark:text-gray-400">
+                <p>Culpa incididunt nisi qui tempor mollit voluptate deserunt eiusmod ut ullamco. Pariatur labore irure tempor dolore est proident adipisicing adipisicing ex elit. Duis ut velit est dolor veniam. Ipsum minim magna deserunt incididunt nulla incididunt. Occaecat dolor occaecat sunt ad nulla esse aute officia anim Lorem officia officia velit. Ex esse labore culpa consectetur nulla elit reprehenderit fugiat cupidatat tempor aute nostrud dolor ea. Aliquip ex voluptate qui tempor culpa eu ea cillum.</p>
+              </ul>
+              <h2 className=" my-2 text-lg font-semibold text-gray-900 dark:text-white font-mono">
+                Reward:some amount
+              </h2>
+            </div>
+<div className="m-5 font-mono underline text-left text-xl hover:text-2xl " >
+            <Link href={"/company"} className="  ">
+              See Details
+            </Link></div>
+          </div>
+          <div className=" m-1 p-4 w-full max-w-sm bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <div className="flex items-baseline text-gray-900 dark:text-white">
+              <span className="text-4xl font-semibold font-mono">
+                Company Name
+              </span>
+            </div>
+            <div className="p-4">
+             
+              <h2 className=" mb-2 text-base text-gray-900 dark:text-white font-mono">
+                Details:
+              </h2>
+              <ul className="font-mono space-y-1 max-w-md list-disc list-inside text-gray-500 dark:text-gray-400">
+                <p>Culpa incididunt nisi qui tempor mollit voluptate deserunt eiusmod ut ullamco. Pariatur labore irure tempor dolore est proident adipisicing adipisicing ex elit. Duis ut velit est dolor veniam. Ipsum minim magna deserunt incididunt nulla incididunt. Occaecat dolor occaecat sunt ad nulla esse aute officia anim Lorem officia officia velit. Ex esse labore culpa consectetur nulla elit reprehenderit fugiat cupidatat tempor aute nostrud dolor ea. Aliquip ex voluptate qui tempor culpa eu ea cillum.</p>
+              </ul>
+              <h2 className=" my-2 text-lg font-semibold text-gray-900 dark:text-white font-mono">
+                Reward:some amount
+              </h2>
+            </div>
+<div className="m-5 font-mono underline text-left text-xl hover:text-2xl " >
+            <Link href={"/company"} className="  ">
+              See Details
+            </Link></div>
+          </div>
+          <div className=" m-1 p-4 w-full max-w-sm bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <div className="flex items-baseline text-gray-900 dark:text-white">
+              <span className="text-4xl font-semibold font-mono">
+                Company Name
+              </span>
+            </div>
+            <div className="p-4">
+             
+              <h2 className=" mb-2 text-base text-gray-900 dark:text-white font-mono">
+                Details:
+              </h2>
+              <ul className="font-mono space-y-1 max-w-md list-disc list-inside text-gray-500 dark:text-gray-400">
+                <p>Culpa incididunt nisi qui tempor mollit voluptate deserunt eiusmod ut ullamco. Pariatur labore irure tempor dolore est proident adipisicing adipisicing ex elit. Duis ut velit est dolor veniam. Ipsum minim magna deserunt incididunt nulla incididunt. Occaecat dolor occaecat sunt ad nulla esse aute officia anim Lorem officia officia velit. Ex esse labore culpa consectetur nulla elit reprehenderit fugiat cupidatat tempor aute nostrud dolor ea. Aliquip ex voluptate qui tempor culpa eu ea cillum.</p>
+              </ul>
+              <h2 className=" my-2 text-lg font-semibold text-gray-900 dark:text-white font-mono">
+                Reward:some amount
+              </h2>
+            </div>
+<div className="m-5 font-mono underline text-left text-xl hover:text-2xl " >
+            <Link href={"/company"} className="  ">
+              See Details
+            </Link></div>
+          </div>
+          <div className=" m-1 p-4 w-full max-w-sm bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <div className="flex items-baseline text-gray-900 dark:text-white">
+              <span className="text-4xl font-semibold font-mono">
+                Company Name
+              </span>
+            </div>
+            <div className="p-4">
+             
+              <h2 className=" mb-2 text-base text-gray-900 dark:text-white font-mono">
+                Details:
+              </h2>
+              <ul className="font-mono space-y-1 max-w-md list-disc list-inside text-gray-500 dark:text-gray-400">
+                <p>Culpa incididunt nisi qui tempor mollit voluptate deserunt eiusmod ut ullamco. Pariatur labore irure tempor dolore est proident adipisicing adipisicing ex elit. Duis ut velit est dolor veniam. Ipsum minim magna deserunt incididunt nulla incididunt. Occaecat dolor occaecat sunt ad nulla esse aute officia anim Lorem officia officia velit. Ex esse labore culpa consectetur nulla elit reprehenderit fugiat cupidatat tempor aute nostrud dolor ea. Aliquip ex voluptate qui tempor culpa eu ea cillum.</p>
+              </ul>
+              <h2 className=" my-2 text-lg font-semibold text-gray-900 dark:text-white font-mono">
+                Reward:some amount
+              </h2>
+            </div>
+<div className="m-5 font-mono underline text-left text-xl hover:text-2xl " >
+            <Link href={"/company"} className="  ">
+              See Details
+            </Link></div>
+          </div>
+          <div className=" m-1 p-4 w-full max-w-sm bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <div className="flex items-baseline text-gray-900 dark:text-white">
+              <span className="text-4xl font-semibold font-mono">
+                Company Name
+              </span>
+            </div>
+            <div className="p-4">
+             
+              <h2 className=" mb-2 text-base text-gray-900 dark:text-white font-mono">
+                Details:
+              </h2>
+              <ul className="font-mono space-y-1 max-w-md list-disc list-inside text-gray-500 dark:text-gray-400">
+                <p>Culpa incididunt nisi qui tempor mollit voluptate deserunt eiusmod ut ullamco. Pariatur labore irure tempor dolore est proident adipisicing adipisicing ex elit. Duis ut velit est dolor veniam. Ipsum minim magna deserunt incididunt nulla incididunt. Occaecat dolor occaecat sunt ad nulla esse aute officia anim Lorem officia officia velit. Ex esse labore culpa consectetur nulla elit reprehenderit fugiat cupidatat tempor aute nostrud dolor ea. Aliquip ex voluptate qui tempor culpa eu ea cillum.</p>
+              </ul>
+              <h2 className=" my-2 text-lg font-semibold text-gray-900 dark:text-white font-mono">
+                Reward:some amount
+              </h2>
+            </div>
+<div className="m-5 font-mono underline text-left text-xl hover:text-2xl " >
+            <Link href={"/company"} className="  ">
+              See Details
+            </Link></div>
+          </div>
+          <div className=" m-1 p-4 w-full max-w-sm bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <div className="flex items-baseline text-gray-900 dark:text-white">
+              <span className="text-4xl font-semibold font-mono">
+                Company Name
+              </span>
+            </div>
+            <div className="p-4">
+             
+              <h2 className=" mb-2 text-base text-gray-900 dark:text-white font-mono">
+                Details:
+              </h2>
+              <ul className="font-mono space-y-1 max-w-md list-disc list-inside text-gray-500 dark:text-gray-400">
+                <p>Culpa incididunt nisi qui tempor mollit voluptate deserunt eiusmod ut ullamco. Pariatur labore irure tempor dolore est proident adipisicing adipisicing ex elit. Duis ut velit est dolor veniam. Ipsum minim magna deserunt incididunt nulla incididunt. Occaecat dolor occaecat sunt ad nulla esse aute officia anim Lorem officia officia velit. Ex esse labore culpa consectetur nulla elit reprehenderit fugiat cupidatat tempor aute nostrud dolor ea. Aliquip ex voluptate qui tempor culpa eu ea cillum.</p>
+              </ul>
+              <h2 className=" my-2 text-lg font-semibold text-gray-900 dark:text-white font-mono">
+                Reward:some amount
+              </h2>
+            </div>
+<div className="m-5 font-mono underline text-left text-xl hover:text-2xl " >
+            <Link href={"/company"} className="  ">
+              See Details
+            </Link></div>
+          </div>
+          <div className=" m-1 p-4 w-full max-w-sm bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <div className="flex items-baseline text-gray-900 dark:text-white">
+              <span className="text-4xl font-semibold font-mono">
+                Company Name
+              </span>
+            </div>
+            <div className="p-4">
+             
+              <h2 className=" mb-2 text-base text-gray-900 dark:text-white font-mono">
+                Details:
+              </h2>
+              <ul className="font-mono space-y-1 max-w-md list-disc list-inside text-gray-500 dark:text-gray-400">
+                <p>Culpa incididunt nisi qui tempor mollit voluptate deserunt eiusmod ut ullamco. Pariatur labore irure tempor dolore est proident adipisicing adipisicing ex elit. Duis ut velit est dolor veniam. Ipsum minim magna deserunt incididunt nulla incididunt. Occaecat dolor occaecat sunt ad nulla esse aute officia anim Lorem officia officia velit. Ex esse labore culpa consectetur nulla elit reprehenderit fugiat cupidatat tempor aute nostrud dolor ea. Aliquip ex voluptate qui tempor culpa eu ea cillum.</p>
+              </ul>
+              <h2 className=" my-2 text-lg font-semibold text-gray-900 dark:text-white font-mono">
+                Reward:some amount
+              </h2>
+            </div>
+<div className="m-5 font-mono underline text-left text-xl hover:text-2xl " >
+            <Link href={"/company"} className="  ">
+              See Details
+            </Link></div>
+          </div>
+          
+          
+          
+          
+         
+          <div className=" m-1 p-4 w-full max-w-sm bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <div className="flex items-baseline text-gray-900 dark:text-white">
+              <span className="text-4xl font-semibold font-mono">
+                Company Name
+              </span>
+            </div>
+            <div className="p-4">
+             
+              <h2 className=" mb-2 text-base text-gray-900 dark:text-white font-mono">
+                Details:
+              </h2>
+              <ul className="font-mono space-y-1 max-w-md list-disc list-inside text-gray-500 dark:text-gray-400">
+                <p>Culpa incididunt nisi qui tempor mollit voluptate deserunt eiusmod ut ullamco. Pariatur labore irure tempor dolore est proident adipisicing adipisicing ex elit. Duis ut velit est dolor veniam. Ipsum minim magna deserunt incididunt nulla incididunt. Occaecat dolor occaecat sunt ad nulla esse aute officia anim Lorem officia officia velit. Ex esse labore culpa consectetur nulla elit reprehenderit fugiat cupidatat tempor aute nostrud dolor ea. Aliquip ex voluptate qui tempor culpa eu ea cillum.</p>
+              </ul>
+              <h2 className=" my-2 text-lg font-semibold text-gray-900 dark:text-white font-mono">
+                Reward:some amount
+              </h2>
+            </div>
+<div className="m-5 font-mono underline text-left text-xl hover:text-2xl " >
+            <Link href={"/company"} className="  ">
+              See Details
+            </Link></div>
+          </div>
+          </div>
+          
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Middle;
---
+++ components/Navbar.js
@@ -0,0 +1,89 @@
+import React, { useRef } from "react";
+import Link from "next/Link";
+import { useEffect, useState } from "react";
+import Web3Modal, { getProviderInfo } from "web3modal";
+import { providers } from "ethers";
+
+const Navbar = () => {
+  const [walletconnected, setWalletconnected] = useState(false);
+  const web3ModalRef = useRef();
+  const [address, setAddress] = useState("")
+  const [ens, setENS] = useState("");
+
+
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    try {
+      const provider = await web3ModalRef.current.connect();
+      const web3Provider = new providers.Web3Provider(provider);
+      const { chainId } = await web3Provider.getNetwork();
+      if (chainId != 5) {
+        window.alert("Change network to goerli");
+        throw new Error("Change network to goerli");
+      }
+      if (needSigner) {
+        const signer = web3Provider.getSigner();
+        return signer;
+      }
+      return web3Provider;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      await getProviderOrSigner();
+      setWalletconnected(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    if (!walletconnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "goerli",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+    }
+  }, [walletconnected]);
+
+  return (
+    <div className="bg-black ">
+      <header className="text-gray-600 body-font ">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <Link href={"/"}>
+            <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+              <span className="ml-3 text-3xl font-mono">REKTIFY</span>
+            </a>
+          </Link>
+          <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center space-x-10 font-mono font-semibold">
+            <Link href={"/about"} className="mr-5 hover:text-gray-900 ">
+              About
+            </Link>
+            <Link href={"/about"} className="mr-5 hover:text-gray-900 ">
+              Something
+            </Link>
+            <Link href={"/about"} className="mr-5 hover:text-gray-900">
+              Something
+            </Link>
+            <Link href={"/about"} className="mr-5 hover:text-gray-900">
+              Something
+            </Link>
+          </nav>
+
+          <button
+            onClick={connectWallet}
+            className="inline-flex font-mono items-center text-white bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500  border-0 py-1 px-3 focus:outline-none hover:bg-orange-400 rounded text-base mt-4 md:mt-0"
+          >
+            Connect your Wallet
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
